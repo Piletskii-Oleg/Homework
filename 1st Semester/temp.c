1f009b19deae30f91026127f9adc92e45b28196f
Piletskii-Oleg <piletskii.oleg@gmail.com>
2021-12-07 15:16:47 +0300

    Update temp.c

--- 1st Semester/temp.c
+++ 1st Semester/temp.c
@@ -1,82 +1,41 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
 struct node
 {
-	int value;
-	struct node* next;
+    int value;
+    struct node* next;
 };
 
 void print_elements(struct node* head)
 {
-	struct node* p = head;
-	while (p != NULL)
-	{
-		printf("%d\n", p->value);
-		p = p->next;
-	}
+    if (head == NULL)
+        return;
+
+    printf("%d\n", head->value);
+    print_elements(head->next);
 }
 
-void add_first(struct node** head, int d)
+struct node* create_first(int value)
 {
-	struct node* p;
-	p = (struct node*)malloc(sizeof(struct node));
-	p->value = d;
-	p->next = *head;
-	*head = p;
-}
-
-void add_new(struct node** current, int value)
-{
-	struct node* newNode;
-	newNode = (struct node*)malloc(sizeof(struct node));
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
 	newNode->value = value;
 	newNode->next = NULL;
-	struct node* last = *current;
-
-	last->next = newNode;
+	return newNode;
 }
 
-void add_last(struct node** prevRef, int d)
+void add_new(struct node* head, int value)
 {
-	struct node* newNode;
-	newNode = (struct node*)malloc(sizeof(struct node));
 
-	struct node* last = *prevRef;
-	newNode->next = NULL;
-	newNode->value = d;
-
-	if (*prevRef == NULL)
-	{
-		*prevRef = newNode;
-		return;
-	}
-
-	last->next = newNode;
-}
-
-struct node* create_node2()
-{
-	int value = 1;
-	struct node** headRef = (struct node**)malloc(sizeof(struct node));
-	printf("0 is the last element of the List\n");
-	*headRef = NULL;
-
-	scanf("%d", &value);
-	add_first(headRef, value);
-	struct node* current = *headRef;
-
-	while (value != 0)
-	{
-		scanf("%d", &value);
-		add_last(current, value);
-		current = current->next;
-	}
-
-	return *headRef;
 }
 
 int main()
 {
-	/*struct node *head;
-	struct node *one = malloc(sizeof(struct node));
+	struct node* head;
+	struct node* one = malloc(sizeof(struct node));
 	struct node* two = malloc(sizeof(struct node));
 	struct node* three = malloc(sizeof(struct node));
 	struct node* four = malloc(sizeof(struct node));
@@ -92,10 +51,5 @@ int main()
 	three->value = 3;
 	four->value = 50;
 
-	struct node** newHead = malloc(sizeof(struct node));
-	*newHead = head;*/
-
-
-	struct node* head = create_node2();
-	print_elements(head);
+    print_elements(head);
 }
