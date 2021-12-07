fd6eafb516b23d281ea886cf6f2645fe0873d67f
Piletskii-Oleg <piletskii.oleg@gmail.com>
2021-12-07 13:48:55 +0300

    Not verified

--- 1st Semester/(44) Linked Lists/Part 2/3.c
+++ 1st Semester/(44) Linked Lists/Part 2/3.c
@@ -1,4 +1,25 @@
-void add_element(struct node* head, int value, int position)
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+struct node
+{
+    int value;
+    struct node* next;
+};
+
+void print_elements(struct node* head)
+{
+    if (head == NULL)
+        return;
+
+    printf("%d\n", head->value);
+    print_elements(head->next);
+}
+
+struct node* add_element(struct node* head, int value, int position)
 {
     struct node* newNode;
     newNode = (struct node*)malloc(sizeof(struct node));
@@ -8,6 +29,7 @@ void add_element(struct node* head, int value, int position)
         newNode->value = value;
         newNode->next = head;
         head = newNode;
+        return head;
     }
     else
     {
@@ -21,22 +43,53 @@ void add_element(struct node* head, int value, int position)
         newNode->next = p->next;
         p->next = newNode;
     }
+
 }
 
 void add_another_element(struct node** headRef, int value, int position)
 {
-    struct node* p;
-    p = (struct node*)malloc(sizeof(struct node));
-    p->value = value;
+    struct node* newNode;
+    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->value = value;
 
     struct node* new = *headRef;
+    struct node** newnew = headRef;
+    int i = 0;
 
-    for (int i = 0; i < position; i++)
+    for (; i < position; i++)
     {
         if (new->next != NULL)
+        {
+            newnew = &((*new).next);
             new = new->next;
+        }
     }
 
-    p->next = new->next;
-    new->next = p;
+    newNode->next = *newnew;
+    *newnew = newNode;
+}
+
+int main()
+{
+	struct node* head;
+	struct node* one = malloc(sizeof(struct node));
+	struct node* two = malloc(sizeof(struct node));
+	struct node* three = malloc(sizeof(struct node));
+	struct node* four = malloc(sizeof(struct node));
+
+	one->next = two;
+	two->next = three;
+	three->next = four;
+	four->next = NULL;
+	head = one;
+
+	one->value = 1;
+	two->value = 2;
+	three->value = 3;
+	four->value = 50;
+
+    print_elements(head);
+    printf("\n");
+    add_another_element(&head, 506, 0);
+    print_elements(head);
 }
