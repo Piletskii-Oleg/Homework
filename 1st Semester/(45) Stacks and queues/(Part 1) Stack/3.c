0ce5aaa7e03ebfb3ad71af24533ba856fc8f495f
Piletskii-Oleg <piletskii.oleg@gmail.com>
2021-12-17 18:02:14 +0300

    Update 3.c

--- 1st Semester/(45) Stacks and queues/(Part 1) Stack/3.c
+++ 1st Semester/(45) Stacks and queues/(Part 1) Stack/3.c
@@ -5,33 +5,74 @@
 #include <string.h>
 #define _CRT_SECURE_NO_WARNINGS
 
+<<<<<<< Updated upstream
 struct Stack
 {
     int data;
     struct Stack* next;
+=======
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct Stack
+{
+    struct node* head;
+>>>>>>> Stashed changes
 };
 
 struct Stack* create_node(int data)
 {
     struct Stack* stackNode = (struct Stack*)malloc(sizeof(struct Stack));
+<<<<<<< Updated upstream
     stackNode->data = data;
     stackNode->next = NULL;
+=======
+    struct node* newNode = (struct Stack*)malloc(sizeof(struct Stack));
+    newNode->data = data;
+    newNode->next = NULL;
+    stackNode->head = newNode;
+}
+
+struct Stack* is_empty(struct Stack* root)
+{
+    return !root;
+>>>>>>> Stashed changes
 }
 
 void push(struct Stack** root, int data)
 {
     struct Stack* newStackNode = create_node(data);
+<<<<<<< Updated upstream
     newStackNode->next = *root;
+=======
+    newStackNode->head->next = *root;
+>>>>>>> Stashed changes
     *root = newStackNode;
 }
 
 int pop(struct Stack** root)
 {
+<<<<<<< Updated upstream
     struct Stack* temp = *root;
     *root = (*root)->next;
     int popped = temp->data;
     free(temp);
 
+=======
+    if (is_empty(*root))
+    {
+        printf("Error. No elements found.\n");
+        return 0;
+    }
+
+    struct Stack* temp = *root;
+    *root = (*root)->head->next;
+    int popped = temp->head->data;
+    free(temp);
+>>>>>>> Stashed changes
     return popped;
 }
 
@@ -52,20 +93,35 @@ int reverse_polish_notation(struct Stack* stack)
         else if (!strcmp(data, "+"))
         {
             temp = pop(&stack);
+<<<<<<< Updated upstream
             stack->data += temp;
             printf("The current number is: %d\n", stack->data);
+=======
+            stack->head->data += temp;
+            printf("The current number is: %d\n", stack->head->data);
+>>>>>>> Stashed changes
         }
         else if (!strcmp(data, "-"))
         {
             temp = pop(&stack);
+<<<<<<< Updated upstream
             stack->data -= temp;
             printf("The current number is: %d\n", stack->data);
+=======
+            stack->head->data -= temp;
+            printf("The current number is: %d\n", stack->head->data);
+>>>>>>> Stashed changes
         }
         else if (!strcmp(data, "*"))
         {
             temp = pop(&stack);
+<<<<<<< Updated upstream
             stack->data *= temp;
             printf("The current number is: %d\n", stack->data);
+=======
+            stack->head->data *= temp;
+            printf("The current number is: %d\n", stack->head->data);
+>>>>>>> Stashed changes
         }
         else if (!strcmp(data, "/"))
         {
@@ -74,8 +130,17 @@ int reverse_polish_notation(struct Stack* stack)
             if (temp == 0)
                 return -1;
 
+<<<<<<< Updated upstream
             stack->data /= temp;
             printf("The current number is: %d\n", stack->data);
+=======
+            stack->head->data /= temp;
+            printf("The current number is: %d\n", stack->head->data);
+        }
+        else if (!strcmp(data, "Stop"))
+        {
+            break;
+>>>>>>> Stashed changes
         }
         else
         {
