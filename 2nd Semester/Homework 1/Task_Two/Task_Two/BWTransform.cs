65a705905d24a12bed695ea7cf8423c6646bf479
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-02-27 12:48:54 +0300

    Created class BWT

---
+++ 2nd Semester/Homework 1/Task_Two/Task_Two/BWTransform.cs
@@ -0,0 +1,115 @@
+﻿namespace Task_2
+{
+    internal static class BWTransform
+    {
+        private static string CycleShift(string input, int offset)
+        {
+            char[] output = new char[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i + offset >= input.Length)
+                {
+                    offset = -i;
+                }
+                output[i] = input[i + offset];
+            }
+            return new string(output);
+        }
+
+        public static string DirectBWT(string input, out int originalLineNumber)
+        {
+            string[] table = new string[input.Length];
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = CycleShift(input, i);
+            }
+            Array.Sort(table);
+
+            char[] result = new char[table.Length];
+            string currentString;
+            originalLineNumber = -1;
+            for (int i = 0; i < table.Length; i++)
+            {
+                currentString = table[i];
+                if (originalLineNumber == -1 && currentString == input)
+                {
+                    originalLineNumber = i;
+                }
+                result[i] = currentString[currentString.Length - 1];
+            }
+            return new string(result);
+        }
+
+        private static string SortString(string input)
+        {
+            char[] array = input.ToCharArray();
+            Array.Sort(array);
+            return new string(array);
+        }
+
+        private static char[] CreateAlphabet(string sortedInput)
+        {
+            int size = 1;
+            for (int i = 0; i < sortedInput.Length - 1; i++)
+            {
+                if (sortedInput[i] != sortedInput[i + 1])
+                {
+                    size++;
+                }
+            }
+            char[] alphabet = new char[size];
+            int currentPos = 0;
+            for (int i = 0; i < sortedInput.Length; i++)
+            {
+                if (!alphabet.Contains(sortedInput[i]))
+                {
+                    alphabet[currentPos] = sortedInput[i];
+                    currentPos++;
+                }
+            }
+            return alphabet;
+        }
+
+        public static string ReverseBWT(string input, int originalLineNumber)
+        {
+            string sortedInput = SortString(input);
+            int[] positionArray = new int[input.Length];
+            for (int i = 0; i < positionArray.Length; i++)
+            {
+                positionArray[i] = -1;
+            }
+            char[] alphabet = CreateAlphabet(sortedInput);
+            int currentPos = 0;
+            int elementPos;
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                elementPos = input.IndexOf(alphabet[i]);
+                for (int j = 0; j < sortedInput.Length; j++)
+                {
+                    if (alphabet[i] == sortedInput[j])
+                    {
+                        while (positionArray[elementPos] != -1 || input[elementPos] != alphabet[i])
+                        {
+                            elementPos++;
+                        }
+                        positionArray[elementPos] = currentPos;
+                        currentPos++;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            currentPos = originalLineNumber;
+            char[] result = new char[input.Length];
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                result[i] = input[currentPos];
+                currentPos = positionArray[currentPos]++;
+            }
+            return new String(result);
+        }
+    }
+}
--- 2nd Semester/Homework 1/Task_Two/Task_Two/Program.cs
+++ 2nd Semester/Homework 1/Task_Two/Task_Two/Program.cs
@@ -1,117 +1,7 @@
-﻿namespace Task_Two
+﻿namespace Task_2
 {
     internal class Program
     {
-        static string CycleShift(string input, int offset)
-        {
-            char[] output = new char[input.Length];
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (i + offset >= input.Length)
-                {
-                    offset = -i;
-                }
-                output[i] = input[i + offset];
-            }
-            return new string(output);
-        }
-
-        static string DirectBWT(string input, out int originalLineNumber)
-        {
-            string[] table = new string[input.Length];
-            for (int i = 0; i < table.Length; i++)
-            {
-                table[i] = CycleShift(input, i);
-            }
-            Array.Sort(table);
-
-            char[] result = new char[table.Length];
-            string currentString;
-            originalLineNumber = -1;
-            for (int i = 0; i < table.Length; i++)
-            {
-                currentString = table[i];
-                if (originalLineNumber == -1 && currentString == input)
-                {
-                    originalLineNumber = i;
-                }
-                result[i] = currentString[currentString.Length - 1];
-            }
-            return new string(result);
-        }
-
-        static string SortString(string input)
-        {
-            char[] array = input.ToCharArray();
-            Array.Sort(array);
-            return new string(array);
-        }
-
-        static char[] CreateAlphabet(string sortedInput)
-        {
-            int size = 1;
-            for (int i = 0; i < sortedInput.Length - 1; i++)
-            {
-                if (sortedInput[i] != sortedInput[i + 1])
-                {
-                    size++;
-                }    
-            }
-            char[] alphabet = new char[size];
-            int currentPos = 0;
-            for (int i = 0; i < sortedInput.Length; i++)
-            {
-                if (!alphabet.Contains(sortedInput[i]))
-                {
-                    alphabet[currentPos] = sortedInput[i];
-                    currentPos++;
-                }
-            }
-            return alphabet;
-        }
-
-        static string ReverseBWT(string input, int originalLineNumber)
-        {
-            string sortedInput = SortString(input);
-            int[] positionArray = new int[input.Length];
-            for (int i = 0; i < positionArray.Length; i++)
-            {
-                positionArray[i] = -1;
-            }
-            char[] alphabet = CreateAlphabet(sortedInput);
-            int currentPos = 0;
-            int elementPos;
-            for (int i = 0; i < alphabet.Length; i++)
-            {
-                elementPos = input.IndexOf(alphabet[i]);
-                for (int j = 0; j < sortedInput.Length; j++)
-                {
-                    if (alphabet[i] == sortedInput[j])
-                    {
-                        while (positionArray[elementPos] != -1 || input[elementPos] != alphabet[i])
-                        {
-                            elementPos++;
-                        }
-                        positionArray[elementPos] = currentPos;
-                        currentPos++;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-            }
-
-            currentPos = originalLineNumber;
-            char[] result = new char[input.Length];
-            for (int i = input.Length - 1; i >= 0; i--)
-            {
-                result[i] = input[currentPos];
-                currentPos = positionArray[currentPos]++;
-            }
-            return new String(result);
-        }
-
         static void Main(string[] args)
         {
             Console.Write("Input text to transform: ");
@@ -121,10 +11,10 @@
                 Console.WriteLine("No text found.");
                 return;
             }
-
-            string converted = DirectBWT(input, out int originalLineNumber);
+            
+            string converted = BWTransform.DirectBWT(input, out int originalLineNumber);
             Console.WriteLine($"The transformed text is: {converted}");
-            string convertedBack = ReverseBWT(converted, originalLineNumber);
+            string convertedBack = BWTransform.ReverseBWT(converted, originalLineNumber);
             Console.WriteLine($"Transformed back: {convertedBack}");
             Console.WriteLine($"Are texts same: {input == convertedBack}");
         }
