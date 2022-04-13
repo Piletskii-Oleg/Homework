09d8e1dcb0f8cadd512bbbbcd4a836720f3604b0
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-04-13 20:40:47 +0300

    Copied code from the presentation and added movement

---
+++ 2nd Semester/Homework 6/ConsoleGame/ConsoleGame/EventLoop.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleGame;
+
+public class EventLoop
+{
+    public event EventHandler<EventArgs> LeftHandler = (sender, args) => { };
+
+    public event EventHandler<EventArgs> RightHandler = (sender, args) => { };
+
+    public event EventHandler<EventArgs> DownHandler = (sender, args) => { };
+
+    public event EventHandler<EventArgs> UpHandler = (sender, args) => { };
+
+    public event EventHandler<EventArgs> EnterHandler = (sender, args) => { };
+
+    public List<EventHandler<EventArgs>> EventHandlers = new ();
+
+    public void Run()
+    {
+        while (true)
+        {
+            var key = Console.ReadKey(true);
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    LeftHandler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.RightArrow:
+                    RightHandler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.Enter:
+                    EnterHandler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.DownArrow:
+                    DownHandler(this, EventArgs.Empty);
+                    break;
+                case ConsoleKey.UpArrow:
+                    UpHandler(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
---
+++ 2nd Semester/Homework 6/ConsoleGame/ConsoleGame/Game.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleGame;
+
+public class Game
+{
+    public void OnLeft(object sender, EventArgs args)
+    {
+        try
+        {
+            Console.CursorLeft--;
+            Console.Write(" ");
+            Console.CursorLeft -= 2;
+            Console.Write("@");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.CursorLeft--;
+            Console.Write("@");
+        }
+    }
+
+    public void OnRight(object sender, EventArgs args)
+    {
+        if (Console.CursorLeft == 0)
+        {
+            Console.Write("@");
+        }
+        else
+        {
+            RemoveCharacter();
+            Console.Write("@");
+        }
+    }
+
+    public void OnDown(object sender, EventArgs args)
+    {
+        RemoveCharacter();
+        Console.CursorLeft--;
+        Console.CursorTop++;
+        Console.Write("@");
+    }
+
+    public void OnUp(object sender, EventArgs args)
+    {
+        try
+        {
+            RemoveCharacter();
+            Console.CursorLeft--;
+            Console.CursorTop--;
+            Console.Write("@");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.Write("@");
+        }
+    }
+
+    private static void RemoveCharacter()
+    {
+        Console.CursorLeft--;
+        Console.Write(" ");
+    }
+}
