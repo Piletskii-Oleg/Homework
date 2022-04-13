2b30efbcb2b55929e7e62b854a2cc8d7b4fd158b
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-04-13 21:00:52 +0300

    Added CI, Fixed warnings and moved Vertex to separate file

---
+++ 2nd Semester/Homework 2/Prefix Tree/Prefix Tree/Vertex.cs
@@ -0,0 +1,32 @@
+﻿namespace PrefixTree;
+
+/// <summary>
+/// Single vertex of a prefix tree.
+/// </summary>
+internal class Vertex
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Vertex"/> class.
+    /// </summary>
+    public Vertex()
+    {
+        this.Next = new Dictionary<char, Vertex>();
+        this.IsTerminal = false;
+        this.HowManyFollow = 0;
+    }
+
+    /// <summary>
+    /// Gets or sets a dictionary which has pointers to the next elements.
+    /// </summary>
+    public Dictionary<char, Vertex> Next { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the vertex is terminal or not.
+    /// </summary>
+    public bool IsTerminal { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating how many words there are that have this vertex in them.
+    /// </summary>
+    public int HowManyFollow { get; set; }
+}
--- 2nd Semester/Homework 2/Prefix Tree/PrefixTree.Tests/PrefixTreeTests.cs
+++ 2nd Semester/Homework 2/Prefix Tree/PrefixTree.Tests/PrefixTreeTests.cs
@@ -1,118 +1,117 @@
+namespace PrefixTree.Tests;
+
 using NUnit.Framework;
 
-namespace PrefixTree.Tests
+public class Tests
 {
-    public class Tests
+    private Trie trie = new();
+
+    [SetUp]
+    public void Setup()
     {
-        private Trie trie = new Trie();
+        trie = new Trie();
+    }
 
-        [SetUp]
-        public void Setup()
-        {
-            trie = new Trie();
-        }
+    [Test]
+    public void AddReturnsTrueAndThenFalseIfInputsRepeat()
+    {
+        Assert.IsTrue(trie.Add("ber"));
+        Assert.IsFalse(trie.Add("ber"));
+    }
 
-        [Test]
-        public void AddReturnsTrueAndThenFalseIfInputsRepeat()
-        {
-            Assert.IsTrue(trie.Add("ber"));
-            Assert.IsFalse(trie.Add("ber"));
-        }
+    [Test]
+    public void SizeReturnsActualValueIfInputsRepeat()
+    {
+        trie.Add("ber");
+        trie.Add("ber");
+        trie.Add("jejr");
+        trie.Add("jejk");
+        Assert.AreEqual(trie.Size, 3);
+    }
 
-        [Test]
-        public void SizeReturnsActualValueIfInputsRepeat()
-        {
-            trie.Add("ber");
-            trie.Add("ber");
-            trie.Add("jejr");
-            trie.Add("jejk");
-            Assert.AreEqual(trie.Size, 3);
-        }
+    [Test]
+    public void RemoveWorksAndDecreasesSize()
+    {
+        trie.Add("ber");
+        trie.Add("rei");
+        Assert.IsTrue(trie.Remove("ber"));
+        Assert.AreEqual(trie.Size, 1);
+    }
 
-        [Test]
-        public void RemoveWorksAndDecreasesSize()
-        {
-            trie.Add("ber");
-            trie.Add("rei");
-            Assert.IsTrue(trie.Remove("ber"));
-            Assert.AreEqual(trie.Size, 1);
-        }
+    [Test]
+    public void RemoveDoesNotWorkTwiceOnTheSameInput()
+    {
+        trie.Add("ber");
+        Assert.IsTrue(trie.Remove("ber"));
+        Assert.IsFalse(trie.Remove("ber"));
+    }
 
-        [Test]
-        public void RemoveDoesNotWorkTwiceOnTheSameInput()
-        {
-            trie.Add("ber");
-            Assert.IsTrue(trie.Remove("ber"));
-            Assert.IsFalse(trie.Remove("ber"));
-        }
+    [Test]
+    public void ContainsWorksProperly()
+    {
+        trie.Add("bobe");
+        trie.Add("gaia");
+        trie.Add("gaio");
+        Assert.IsTrue(trie.Contains("bobe"));
+        Assert.IsTrue(trie.Contains("gaia"));
+        Assert.IsTrue(trie.Contains("gaio"));
+    }
 
-        [Test]
-        public void ContainsWorksProperly()
-        {
-            trie.Add("bobe");
-            trie.Add("gaia");
-            trie.Add("gaio");
-            Assert.IsTrue(trie.Contains("bobe"));
-            Assert.IsTrue(trie.Contains("gaia"));
-            Assert.IsTrue(trie.Contains("gaio"));
-        }
+    [Test]
+    public void WordIsNotRemovedIfItIsAPrefix()
+    {
+        trie.Add("bebe");
+        trie.Remove("ber");
+        Assert.IsTrue(trie.Contains("bebe"));
+    }
 
-        [Test]
-        public void WordIsNotRemovedIfItIsAPrefix()
-        {
-            trie.Add("bebe");
-            trie.Remove("ber");
-            Assert.IsTrue(trie.Contains("bebe"));
-        }
+    [Test]
+    public void PrefixOfTheWordIsNotRemoved()
+    {
+        trie.Add("beb");
+        trie.Add("bebe");
+        trie.Remove("bebe");
+        Assert.IsTrue(trie.Contains("beb"));
+    }
 
-        [Test]
-        public void PrefixOfTheWordIsNotRemoved()
-        {
-            trie.Add("beb");
-            trie.Add("bebe");
-            trie.Remove("bebe");
-            Assert.IsTrue(trie.Contains("beb"));
-        }
+    [Test]
+    public void TrieCanContainSingleLetterWords()
+    {
+        trie.Add("e");
+        trie.Add("a");
+        Assert.IsTrue(trie.Contains("e"));
+        Assert.IsTrue(trie.Contains("a"));
+        Assert.AreEqual(trie.Size, 2);
+    }
 
-        [Test]
-        public void TrieCanContainSingleLetterWords()
-        {
-            trie.Add("e");
-            trie.Add("a");
-            Assert.IsTrue(trie.Contains("e"));
-            Assert.IsTrue(trie.Contains("a"));
-            Assert.AreEqual(trie.Size, 2);
-        }
+    [Test]
+    public void TwoConsecutiveRemovesWithSuffixesWork()
+    {
+        trie.Add("ber");
+        trie.Add("berk");
+        Assert.IsTrue(trie.Remove("berk"));
+        Assert.IsTrue(trie.Remove("ber"));
+    }
 
-        [Test]
-        public void TwoConsecutiveRemovesWithSuffixesWork()
-        {
-            trie.Add("ber");
-            trie.Add("berk");
-            Assert.IsTrue(trie.Remove("berk"));
-            Assert.IsTrue(trie.Remove("ber"));
-        }
+    [Test]
+    public void HowManyStartsWithPrefixWorksRight()
+    {
+        trie.Add("ber");
+        trie.Add("berk");
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 2);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("ber"), 2);
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("berk"), 1);
+    }
 
-        [Test]
-        public void HowManyStartsWithPrefixWorksRight()
-        {
-            trie.Add("ber");
-            trie.Add("berk");
-            Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 2);
-            Assert.AreEqual(trie.HowManyStartsWithPrefix("ber"), 2);
-            Assert.AreEqual(trie.HowManyStartsWithPrefix("berk"), 1);
-        }
-
-        [Test]
-        public void HowManyStartsWithPrefixWorksRightAfterRemoval()
-        {
-            trie.Add("ber");
-            trie.Add("bear");
-            trie.Add("berk");
-            trie.Remove("berk");
-            Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 2);
-            trie.Remove("bear");
-            Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 1);
-        }
+    [Test]
+    public void HowManyStartsWithPrefixWorksRightAfterRemoval()
+    {
+        trie.Add("ber");
+        trie.Add("bear");
+        trie.Add("berk");
+        trie.Remove("berk");
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 2);
+        trie.Remove("bear");
+        Assert.AreEqual(trie.HowManyStartsWithPrefix("be"), 1);
     }
 }
