2e7436248c3cb16ca32a45c3c87d37a48baeda7c
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-04-18 21:31:16 +0300

    Fixed codacy issue

--- 2nd Semester/Homework 6/ConsoleGame/ConsoleGame/EventLoop.cs
+++ 2nd Semester/Homework 6/ConsoleGame/ConsoleGame/EventLoop.cs
@@ -57,8 +57,6 @@ public class EventLoop
                     this.EscHandler(this, EventArgs.Empty);
                     isRunning = false;
                     break;
-                default:
-                    break;
             }
         }
     }
