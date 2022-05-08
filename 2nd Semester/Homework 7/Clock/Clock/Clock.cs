358b078f3675cd38de53f9ddd9bed8b98a8c75fc
Piletskii-Oleg <piletskii.oleg@gmail.com>
2022-05-08 16:42:58 +0300

    Implemented clock

--- 2nd Semester/Homework 7/Clock/Clock/Clock.cs
+++ 2nd Semester/Homework 7/Clock/Clock/Clock.cs
@@ -5,39 +5,36 @@ public partial class Clock : Form
     public Clock()
     {
         InitializeComponent();
+        pictureBox1.Paint += new PaintEventHandler(DrawHands);
+        Controls.Add(pictureBox1);
+        center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
     }
 
-    private int hours = DateTime.Now.Hour;
-    private int minutes = DateTime.Now.Minute;
-    private int seconds = DateTime.Now.Second;
+    private Point center;
 
-    private void DrawHands(PaintEventArgs e)
+    private void DrawHands(object? sender, PaintEventArgs e)
     {
-        
+        int hours = DateTime.Now.Hour;
+        int minutes = DateTime.Now.Minute;
+        int seconds = DateTime.Now.Second;
+
+        double hourAngle = ((hours % 12) + (double)minutes / 60) * 30 * Math.PI / 180;
+        double minuteAngle = (minutes + (double)seconds / 60) * 6 * Math.PI / 180;
+        double secondAngle = seconds * 6 * Math.PI / 180;
+
+        UpdateHand(e, hourAngle, 50, Color.Red);
+        UpdateHand(e, minuteAngle, 80, Color.Blue);
+        UpdateHand(e, secondAngle, 90, Color.Orange);
     }
 
-    private void TimerTick(object sender, EventArgs e)
+    private void UpdateHand(PaintEventArgs e, double angle, int handLength, Color color)
     {
-        
-        if (seconds == 59)
-        {
-            minutes++;
-            seconds = 0;
-        }
-        else
-        {
-            seconds++;
-        }
+        e.Graphics.DrawLine(new Pen(color), center,
+            new Point((int)(pictureBox1.Width / 2 + handLength * Math.Sin(angle)), (int)(pictureBox1.Height / 2 - handLength * Math.Cos(angle))));
+    }
 
-        if (minutes == 59 && seconds == 59)
-        {
-            minutes = 0;
-            hours++;
-        }
-
-        if (hours == 24 && minutes == 59 && seconds == 59)
-        {
-            hours = 0;
-        }
+    private void TimerTick(object? sender, EventArgs e)
+    {
+        pictureBox1.Refresh();
     }
 }
